d4d11806b9210ab6119b59492bdcb8681aa7a2fa
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-11-03 16:45:47 +0200

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,7 @@
 # installs wordpress
 
-include_recipe 'wordpress'
+include_recipe 'shlomo-wordpress'
 package "php5-curl" # W3TC needs this
 include_recipe "apache2::mod_expires"
-include_recipe 'chef-newrelic-sysmond'
+include_recipe 'shlomo-newrelic'
 
