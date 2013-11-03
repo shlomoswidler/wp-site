2c809fe89782ef6f3f71567cbc2eef3e39047664
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-11-03 16:46:34 +0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,8 @@ description      'Installs/Configures wp-site'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-recipe 'default', "Install/Configure wo-site"
+recipe 'default', "Install/Configure wp-site"
 
-depends 'wordpress', "= 1.2.2"
-depends 'chef-newrelic-sysmond'
+depends 'shlomo-wordpress', "= 0.1.0"
+depends 'shlomo-newrelic', "= 0.1.0"
 depends 'apache2', '= 0.9'
