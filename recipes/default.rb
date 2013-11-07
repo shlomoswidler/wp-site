c0b0843f0e1378f72a7c44be30320b767c65633e
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-11-07 15:33:25 +0200

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,10 @@
 include_recipe 'annoyances::ubuntu'
 include_recipe 'shlomo-wordpress'
 package "php5-curl" # W3TC needs this
+package 'php5-memcached'
+package 'php5-tidy'
+package 'php-apc'
 include_recipe "apache2::mod_expires"
 include_recipe 'shlomo-newrelic'
+include_recipe 'shlomo-newrelic::php'
 
