7c7b158fa7f3d0abc811f39560e78317374d83ca
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-11-11 16:45:22 +0200

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,37 +1,5 @@
 # installs wordpress
 
-#turn off services
-%w{'apparmor', 'whoopsie'}.each do |svc|
-  service svc do
-    action [:stop,:disable]
-    ignore_failure true
-  end
-end
+include_recipe 'prepare_environment'
 
-#turn off byobu
-file "/etc/profile.d/Z98-byobu" do
-  action :delete
-  not_if do
-    node.recipe?("byobu")
-  end
-end
-
-%w{popularity-contest unity-lens-shopping whoopsie}.each do |pkg|
-  package pkg do
-    action :purge
-    ignore_failure true
-  end
-end
-
-include_recipe 'shlomo-wordpress'
-
-# W3TC needs these
-package "php5-curl" 
-package 'php5-memcache'
-package 'php5-tidy'
-package 'php-apc'
-include_recipe "apache2::mod_expires"
-
-include_recipe 'shlomo-newrelic'
-include_recipe 'shlomo-newrelic::php'
-
+include_recipe 'deploy_wordpress'
