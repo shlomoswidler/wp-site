cd90560472bddbfd514a1db0b8c48bd48b49aafe
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-11-17 16:15:17 +0200

    Update prepare_environment.rb

--- recipes/prepare_environment.rb
+++ recipes/prepare_environment.rb
@@ -21,27 +21,6 @@ end
   end
 end
 
-execute "apt-get update" do
-  action :nothing
-end
-
-repo=`grep 'deb http.*precise main' /etc/apt/sources.list | cut -d' ' -f 2`
-["precise multiverse", "precise-updates multiverse"].each do |expr|
-  execute "add #{expr} deb repository" do
-    user 'root'
-    group 'root'
-    command <<-EOF
-      echo 'deb #{repo} #{expr}' >> /etc/apt/sources.list.d/multiverse.list
-    EOF
-    not_if do
-      find = Mixlib::ShellOut.new("find /etc/apt/ -name '*.list' | xargs cat | grep  ^[[:space:]]*deb[[:space:]].*#{expr}").run_command
-      find.exitstatus && find.stdout.length>1
-    end
-    action :nothing
-    notifies :run, "execute[apt-get update]", :immediately
-  end.run_action(:run)
-end
-
 include_recipe "mysql::client"
 
 pkg = value_for_platform(
@@ -58,6 +37,8 @@ end
 include_recipe "apache2::default"
 include_recipe "apache2::mod_php5"
 include_recipe "apache2::mod_ssl"
+include_recipe "apache2::mod_fcgid"
+package 'php5-fpm'
 
 # W3TC needs these
 package "php5-curl" 
@@ -68,11 +49,7 @@ include_recipe "apache2::mod_deflate"
 include_recipe "apache2::mod_headers"
 include_recipe "apache2::mod_expires"
 include_recipe "apache2::mod_rewrite"
-include_recipe "apache2::mod_"
 include_recipe "apache2::mod_php5_apache"
-
-package 'php5-fpm'
-package 'libapache2-mod-fastcgi'
 
 include_recipe 'shlomo-newrelic'
 include_recipe 'shlomo-newrelic::php'
