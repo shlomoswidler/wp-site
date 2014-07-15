597b06e4979f5001b7682f6041ee7dba9c7ebeb1
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-07-15 19:24:21 +0300

    Bump version, add dependency on newrelic_plugins

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'shlomo.swidler@orchestratus.com'
 license          'All rights reserved'
 description      'Installs/Configures wp-site'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 recipe 'default', "Install/Configure wp-site"
 
@@ -13,3 +13,4 @@ supports "ubuntu"
 depends 'shlomo-wordpress', "= 0.1.0"
 depends 'shlomo-newrelic', "= 0.2.0"
 depends 'apache2', '>= 1.0'
+depends 'newrelic_plugins', '>= 1.0.1'
